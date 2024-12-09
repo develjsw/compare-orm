ba37a24b8972467391e95e666c2165487a6469ca
jsw <develjsw@gmail.com>
2024-12-09 23:55:18 +0900

    상품 전체 조회 API - test case, 예외처리 추가

--- src/orm-prisma/goods/goods.controller.ts
+++ src/orm-prisma/goods/goods.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { tb_goods as Goods } from '@prisma/client';
 
 @Controller('orm-prisma/goods')
 export class GoodsController {
@@ -7,6 +8,13 @@ export class GoodsController {
 
     @Get()
     async findGoodsAll() {
-        return await this.prisma.tb_goods.findMany();
+        const goodsList: Goods[] = await this.prisma.tb_goods.findMany();
+        // const testCase1 = goodsList.filter((goods: Goods) => goods.member_id == 1) // goods 모델 객체에 member_id는 없으므로 Error 발생
+
+        if (!goodsList.length) {
+            throw new NotFoundException();
+        }
+
+        return goodsList;
     }
 }
--- src/orm-typeorm/goods/servicies/goods.service.ts
+++ src/orm-typeorm/goods/servicies/goods.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { GoodsRepository } from '../repositories/goods.repository';
 import { GoodsEntity } from '../../entities/mysql/goods.entity';
 
@@ -7,6 +7,13 @@ export class GoodsService {
     constructor(private readonly goodsRepository: GoodsRepository) {}
 
     async findGoodsAll(): Promise<GoodsEntity[]> {
-        return await this.goodsRepository.findGoodsAll();
+        const goodsList: GoodsEntity[] = await this.goodsRepository.findGoodsAll();
+        // const testCase1 = goodsList.filter((goods: GoodsEntity) => goods.member_id == 1) // goods 모델 객체에 member_id는 없으므로 Error 발생
+
+        if (!goodsList.length) {
+            throw new NotFoundException();
+        }
+
+        return goodsList;
     }
 }
